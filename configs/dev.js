9596d32919c1df8513d4c50886073ea73676c343
Armond <armondbaghumyan94@gmail.com>
2019-11-06 20:53:11 +0400

    06.11.2019

--- configs/dev.js
+++ configs/dev.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
 mongoose.set('useFindAndModify', false);
-
-mongoose.connect('mongodb://localhost:27017/beeweb_intern', {useNewUrlParser: true, useUnifiedTopology: true}, err => {
+//'mongodb://localhost:27017/beeweb_intern'
+mongoose.connect('mongodb://beeweb_insterns:beeweb_insterns12345@ds059722.mlab.com:59722/beeweb_intern', {useNewUrlParser: true, useUnifiedTopology: true}, err => {
   if (err) {
     throw err;
   };
 
-  console.log('successfuly connected');
+  console.log('successfuly connected to dev db');
 });
--- configs/prod.js
+++ configs/prod.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
 mongoose.set('useFindAndModify', false);
-
+//`mongodb://${process.env.mongo_host}:${process.env.mongo_port}/beeweb_intern`
 mongoose.connect(`mongodb://${process.env.mongo_host}:${process.env.mongo_port}/beeweb_intern`, {useNewUrlParser: true, useUnifiedTopology: true}, err => {
   if (err) {
     throw err;
   };
 
-  console.log('successfuly connected');
+  console.log('successfuly connected to prod db');
 });

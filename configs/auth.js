59b253bae182c5dbde8a77c40fe4bf0a745cfb25
Armond <armondbaghumyan94@gmail.com>
2019-11-13 04:09:40 +0400

    add auth middleware

--- configs/auth.js
+++ configs/auth.js
@@ -4,6 +4,9 @@ const mongoose = require('mongoose');
 
 module.exports = async (req, res, next) => {
   try {
+    if (!req.headers.authorization) {
+      throw new Error('!sing in');
+    }
     const token = req.headers.authorization.split(' ')[1];
     const decoded = jwt.verify(token, process.env.JWT_KEY);
     if (!decoded) {

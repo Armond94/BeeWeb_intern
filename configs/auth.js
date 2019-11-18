1d04137c1b304f8c5897d3e295487602513538dd
Armond <armondbaghumyan94@gmail.com>
2019-11-18 21:43:11 +0400

    test auth

--- configs/auth.js
+++ configs/auth.js
@@ -4,15 +4,18 @@ const mongoose = require('mongoose');
 
 module.exports = async (req, res, next) => {
   try {
+    console.log('111111111', req.headers.authorization);
     if (!req.headers.authorization) {
       throw new Error('!sing in');
     }
     const token = req.headers.authorization.split(' ')[1];
     const decoded = jwt.verify(token, process.env.JWT_KEY);
+    console.log('2222222222', req.headers.authorization);
     if (!decoded) {
       throw new Error('!sing in');
     }
     let user = await users.findOne({_id: decoded.userId, deletedAt: null});
+    console.log('33333333', req.headers.authorization);
 
     req.user = user;
     next();
